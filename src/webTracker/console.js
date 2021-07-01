121ff1c4f2700a8096d8d028150040bfa90beeb7
wangfei <wangfei@eshippinggateway.com>
2021-07-01 10:14:26 +0800

    update

--- src/webTracker/console.js
+++ src/webTracker/console.js
@@ -1,14 +1,26 @@
 import log from './log';
+import { getStackLines } from '../util/index';
 
 export function injectConsoleError() {
     console.error = function (origin) {
         return function (info) {
-            Log.send({
-                kind: 'STABILITY',
-                type: 'ERROR',
-                errorType: 'CONSOLE_ERROR',
-                desc: info
-            })
+            if (typeof info === 'string') {
+                log.send({
+                    kind: 'STABILITY',
+                    type: 'ERROR',
+                    errorType: 'CONSOLE_ERROR',
+                    message: info
+                })
+            } else if (typeof info === 'object') {
+                log.send({
+                    kind: 'STABILITY',
+                    type: 'ERROR',
+                    errorType: 'CONSOLE_ERROR',
+                    message: info.message,
+                    stack: getStackLines(info.stack)
+                })
+            }
+
             origin.call(console, info);
         };
     }(console.error);
