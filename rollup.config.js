158e46103cc7245da9c938fd4a6cf7f062fb9364
wangfei <927230462@qq.com>
2023-08-01 16:35:44 +0800

    uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import {uglify} from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 
 export default {
-    input: './src/index.js',
-    output: {
-        file: './dist/web-tracker.min.js',
-        name: 'webTracker',
-        format: 'iife'
-    },
-    plugins:[
-        // uglify(),
-    ]
+  input: './src/index.js',
+  output: {
+    file: './dist/web-tracker.min.js',
+    name: 'webTracker',
+    format: 'iife'
+  },
+  plugins: [
+    uglify()
+  ]
 }
